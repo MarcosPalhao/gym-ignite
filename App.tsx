de7eda2c948ccf624719d84b0423b6dd49e7c45b
Marcos Vinicius <marcos.palhao18@gmail.com>
2023-10-23 11:18:31 -0300

    add Default Theme

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
-import { Text, View } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import {NativeBaseProvider} from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
+import { THEME } from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts([ Roboto_400Regular, Roboto_700Bold ]);
 
   return (
-    <View>
-      {fontsLoaded ? <Text>Hello world</Text> : <View />}
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <View /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
 
