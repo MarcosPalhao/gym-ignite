8f146ab3058fa423e1c22e69e3bea4572c601e8f
Marcos Vinicius <marcos.palhao18@gmail.com>
2023-10-23 11:47:08 -0300

    add background image on SiginIn Screen

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import {NativeBaseProvider} from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
 import { THEME } from './src/theme';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts([ Roboto_400Regular, Roboto_700Bold ]);
@@ -15,7 +16,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <View /> : <Loading />}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
