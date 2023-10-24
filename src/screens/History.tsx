31d67264f4e9ac9a32bb527944023bddaafd3b4a
Marcos Vinicius <marcos.palhao18@gmail.com>
2023-10-24 15:22:57 -0300

    style bottom navigator

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -3,7 +3,7 @@ import {Center, Text} from 'native-base'
 export function History() {
     return (
         <Center flex={1}>
-            <Text color="white">Home</Text>
+            <Text color="white">History</Text>
         </Center>
     );
 }
