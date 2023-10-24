b930a14a5db6385e949aa783eec3fbfd1c0392b9
Marcos Vinicius <marcos.palhao18@gmail.com>
2023-10-24 10:38:26 -0300

    fix misc

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -4,13 +4,21 @@ import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 import { Input } from '@components/input';
 import { Button } from '@components/Button';
+import { useNavigation } from '@react-navigation/native';
 
 export function SignUp() {
+    const navigation = useNavigation();
+
+    function handleGoBack() {
+        navigation.goBack();
+    }
+
     return (
         <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
-            <VStack flex={1} bg="gray.700" px={10}>
+            <VStack flex={1} px={10}>
                 <Image 
                     source={BackgroundImg}
+                    defaultSource={BackgroundImg}
                     alt="Pessoas treinando"
                     resizeMode='contain'
                     position="absolute"
@@ -51,6 +59,7 @@ export function SignUp() {
                     title='Voltar para o login' 
                     variant="outline" 
                     mt={24}
+                    onPress={handleGoBack}
                 />
             </VStack>
         </ScrollView>
